f0a8e1e1b153f85a25217b0092dd930ea80dc7ac
Avinash-2401 <arpytho665@gmail.com>
2026-02-13 00:45:54 +0530

    Day 2

--- Spring-Security-Demo/src/main/java/com/Security/Spring_Security_Demo/SecurityConfig.java
+++ Spring-Security-Demo/src/main/java/com/Security/Spring_Security_Demo/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +20,15 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authorizeRequest-> authorizeRequest.anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User.withUsername("user1").password("{noop}user@123").build();
+        UserDetails user2 = User.withUsername("user2").password("{noop}user@234").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}admin@123").build();
+
+         return new InMemoryUserDetailsManager(user1,user2,admin);
+
     }
 }
