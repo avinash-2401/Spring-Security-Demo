53b895007f3489e521155c0eed746245bb25f82e
Avinash-2401 <arpytho665@gmail.com>
2026-02-13 16:38:25 +0530

    Day 3

--- Spring-Security-Demo/src/main/java/com/Security/Spring_Security_Demo/HelloController.java
+++ Spring-Security-Demo/src/main/java/com/Security/Spring_Security_Demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.Security.Spring_Security_Demo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,18 @@ public class HelloController {
 
 
     @GetMapping("hello")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     public String sayHello(){
-        return "Avinash";
+        return "Avinash ,normal ....!!";
     }
 
+    @GetMapping("/admin/hello")
+    public String sayAdminHello(){
+        return "Avinash, Admin !!";
+    }
+
+    @GetMapping("/user/hello")
+    public String sayUserHello(){
+        return "Avinash,User !!!!";
+    }
 }
--- Spring-Security-Demo/src/main/java/com/Security/Spring_Security_Demo/SecurityConfig.java
+++ Spring-Security-Demo/src/main/java/com/Security/Spring_Security_Demo/SecurityConfig.java
@@ -1,34 +1,79 @@
 package com.Security.Spring_Security_Demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
-        http.authorizeHttpRequests(authorizeRequest-> authorizeRequest.anyRequest().authenticated());
-        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(authorizeRequest->
+                        authorizeRequest.requestMatchers("/admin/**").hasRole("ADMIN")
+                                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+
+                                .anyRequest().authenticated());
+                                http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user1= User.withUsername("user1").password("{noop}user@123").build();
-        UserDetails user2 = User.withUsername("user2").password("{noop}user@234").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}admin@123").build();
 
-         return new InMemoryUserDetailsManager(user1,user2,admin);
+        UserDetails user1= User.withUsername("user1")
+                //.password("{noop}user@123")
+                .password(passwordEncoder().encode("user@123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("user2")
+                //.password("{noop}user@234")
+                .password(passwordEncoder().encode("user@234"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                //.password("{noop}admin@123")
+                .password(passwordEncoder().encode("admin@123"))
+                .roles("ADMIN")
+                .build();
+
+         //return new InMemoryUserDetailsManager(user1,user2,admin);
+        JdbcUserDetailsManager userDetailsManager=new JdbcUserDetailsManager(dataSource);
+
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(user2);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+
+
 
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
